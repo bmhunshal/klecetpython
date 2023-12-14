dd37cd9603b9f0ff5e30f1bde3cd00744cd97fb6
bmhunshal <b.hunshal22@gmail.com>
2023-12-14 09:11:12 +0530

    Adding dictionary.py file

---
+++ dictionary.py
@@ -0,0 +1,31 @@
+# d = {}
+# print(dir(d))
+#
+# d3 = {1:'Apple', 2: 'Banana', 3: 'Cucumber', 4: 'Grapes'}
+# print(d3)
+# d3[5] = 'Mango'
+# print(d3)
+# d3.update({6: 'Kiwi'})
+#
+# d3.update({3:'Cherry'})
+# print(d3)
+# d3.update({4:"Dragon Fruit"})
+# d3.update({5:'Egg Fruit'})
+#
+# print(d3)
+#
+# print(d3.pop(1))
+# print(d3.popitem())
+# print("\n get 5th Item: ", d3.get(5))
+#
+# d2 = {'Fruits': ['Mango', "Guarva", "Grapes"], "vegetables": ["Tomato", "Onion", "brinjal"]}
+#
+# print(d2['Fruits'])
+# --------Range() Function-----------------------
+
+# for i in range(10,0,-1):
+#     print(i)
+for i in range(0,10,1):
+    print(i)
+for i in range(10):
+    print(i)
